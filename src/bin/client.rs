1130cc7d6e40153b15b9a4e8e207872b44abe5b5
Krio Lyth <kriolyth@gmail.com>
2021-09-18 23:46:39 +0200

    Grumpy protocol initiated

--- src/bin/client.rs
+++ src/bin/client.rs
@@ -1,6 +1,26 @@
-use grumpc::{Empty};
-use grumpc::grumpy_client::{GrumpyClient};
+use grumpc::grumpy_client::GrumpyClient;
+use grumpc::{Empty, Item};
 
+use chrono::prelude::*;
+
+fn make_item() -> Item {
+    Item {
+        mood: "grumpy".to_owned(),
+        contents_sentiment: "disappointment".to_owned(),
+        full_text: "no".to_owned(),
+    }
+}
+
+macro_rules! measure {
+    ($f: expr) => {{
+        let start_time = Utc::now();
+        for _ in 0..1000 {
+            $f;
+        }
+        let duration = Utc::now() - start_time;
+        duration.num_milliseconds() as f64 / 1000.
+    }};
+}
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
@@ -9,6 +29,15 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let mut client = GrumpyClient::connect("http://localhost:3002").await?;
     let res = client.status(Empty {}).await?.into_inner();
     println!("Server status: {}", res.success);
+
+    // let res = client.good_enough(make_item()).await?.into_inner();
+    // println!("Good enough: {}", res.good_enough);
+    let elapsed = measure!(client.good_enough(make_item()).await?);
+    println!(
+        "Elapsed {} s, throughput: {:.2} req/s",
+        elapsed,
+        1000. / elapsed
+    );
 
     Ok(())
 }
--- src/bin/server.rs
+++ src/bin/server.rs
@@ -1,18 +1,30 @@
-use grumpc::{Empty, Item, StatusReply, GrumpyReply};
 use grumpc::grumpy_server::{Grumpy, GrumpyServer};
+use grumpc::{Empty, GrumpyReply, Item, StatusReply};
 use tonic::{async_trait, Request, Response};
 
 struct Grumper;
 
+impl Grumper {
+    fn is_item_good(item: &Item) -> bool {
+        item.mood == "grumpy" && item.contents_sentiment == "disappointment"
+    }
+}
+
 #[async_trait]
 impl Grumpy for Grumper {
-    async fn status(&self, _request: Request<Empty>)
-    -> Result<Response<StatusReply>, tonic::Status> {
-        Ok(tonic::Response::new(StatusReply {success: true}))
+    async fn status(
+        &self,
+        _request: Request<Empty>,
+    ) -> Result<Response<StatusReply>, tonic::Status> {
+        Ok(Response::new(StatusReply { success: true }))
     }
-    async fn good_enough(&self, _request: Request<Item>) ->
-        Result<Response<GrumpyReply>, tonic::Status> {
-        Err(tonic::Status::unimplemented("Method not implemented"))
+    async fn good_enough(
+        &self,
+        request: Request<Item>,
+    ) -> Result<Response<GrumpyReply>, tonic::Status> {
+        Ok(Response::new(GrumpyReply {
+            good_enough: Grumper::is_item_good(&request.into_inner()),
+        }))
     }
 }
 
@@ -20,7 +32,7 @@ impl Grumpy for Grumper {
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     println!("Hello, server!");
 
-    let addr = std::net::SocketAddr::from(([0,0,0,0], 3002));
+    let addr = std::net::SocketAddr::from(([0, 0, 0, 0], 3002));
     let service = tonic::transport::Server::builder()
         .add_service(GrumpyServer::new(Grumper {}))
         .serve(addr);
