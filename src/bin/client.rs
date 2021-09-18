7edfe8b754b6a2652cefb8ea61c5f2d83ff80ae7
Krio Lyth <kriolyth@gmail.com>
2021-09-18 21:01:57 +0200

    Add grpc communication

--- src/bin/client.rs
+++ src/bin/client.rs
@@ -1,3 +1,14 @@
-fn main() {
+use grumpc::{Empty};
+use grumpc::grumpy_client::{GrumpyClient};
+
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
     println!("Hello, client!");
+
+    let mut client = GrumpyClient::connect("http://localhost:3002").await?;
+    let res = client.status(Empty {}).await?.into_inner();
+    println!("Server status: {}", res.success);
+
+    Ok(())
 }
--- src/bin/server.rs
+++ src/bin/server.rs
@@ -1,3 +1,30 @@
-fn main() {
+use grumpc::{Empty, Item, StatusReply, GrumpyReply};
+use grumpc::grumpy_server::{Grumpy, GrumpyServer};
+use tonic::{async_trait, Request, Response};
+
+struct Grumper;
+
+#[async_trait]
+impl Grumpy for Grumper {
+    async fn status(&self, _request: Request<Empty>)
+    -> Result<Response<StatusReply>, tonic::Status> {
+        Ok(tonic::Response::new(StatusReply {success: true}))
+    }
+    async fn good_enough(&self, _request: Request<Item>) ->
+        Result<Response<GrumpyReply>, tonic::Status> {
+        Err(tonic::Status::unimplemented("Method not implemented"))
+    }
+}
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
     println!("Hello, server!");
+
+    let addr = std::net::SocketAddr::from(([0,0,0,0], 3002));
+    let service = tonic::transport::Server::builder()
+        .add_service(GrumpyServer::new(Grumper {}))
+        .serve(addr);
+    service.await?;
+
+    Ok(())
 }
